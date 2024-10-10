0fc4206a897f45c847ea8bd1fae44457429946d6
juanlozano3 <j.lozanog@uniandes.edu.co>
2024-10-10 00:20:52 -0500

    health-check

--- usuario/urls.py
+++ usuario/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.user_home, name='user_home'),
     path('actualizar/', views.actualizar_usuario, name='actualizar_usuario'),  # Nueva ruta para la vista 'actualizar_usuario'
+    path("health-check/", views.health_check),
 ]
