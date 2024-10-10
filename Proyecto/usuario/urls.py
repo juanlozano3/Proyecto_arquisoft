16ebc3c234e1e5a74baf881afe615740d3a2c005
juanlozano3 <j.lozanog@uniandes.edu.co>
2024-10-09 21:07:08 -0500

    html usairos

---
+++ Proyecto/usuario/urls.py
@@ -0,0 +1,8 @@
+# usuario/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.user_home, name='user_home'),
+    path('actualizar/', views.actualizar_usuario, name='actualizar_usuario'),  # Nueva ruta para actualizar
+]
